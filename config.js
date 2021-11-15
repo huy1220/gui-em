b4b6b44ca20733ae11ba99f47ae713f9a6cb2770
huy1220 <daohuy2000@gmail.com>
2021-11-16 00:26:14 +0700

    hi
    hi

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'HE LUU!',
+    introDesc: ` Tớ có điều muốn nói với cậu rất lâu rồi, ấn vào bên dưới để xem nhé`,
+    btnIntro: 'Ấn đi tình yêu',
+    title: 'Phải chăng em đã nợ anh tiền mà em không nhớ không 😙',
+    desc: 'Phải chăng em muốn trả anh ngay bây giờ không ',
+    btnYes: 'Dạ đúng rồi ạ !!',
+    btnNo: 'Không phải, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại không trả tiền anh',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Do anh đẹp trai quá làm em quên, e bắn khoản ngay đây ạ!!',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Còn đợi gì mà không nhắn tin cho anh :v.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/wenlang.anhduong/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
